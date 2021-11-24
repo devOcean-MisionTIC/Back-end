0f34a43090ff3d350980cf82a3814a5e46a50c7f
Felipe Sierra <felipesierrae@gmail.com>
2021-11-23 20:25:28 -0500

    Correcion query proyectos y adicion para autenticacion

--- models/usuario/tipos.js
+++ models/usuario/tipos.js
@@ -24,6 +24,7 @@ const tiposUsuario = gql`
       correo: String!
       rol: Enum_Rol!
       estado: Enum_EstadoUsuario
+      password: String!
     ): Usuario
 
     editarUsuario(
@@ -32,8 +33,7 @@ const tiposUsuario = gql`
       apellido: String!
       identificacion: String!
       correo: String!
-      rol: Enum_Rol!
-      estado: Enum_EstadoUsuario
+      estado: Enum_EstadoUsuario!
     ): Usuario
 
     eliminarUsuario(_id: String, correo: String): Usuario
