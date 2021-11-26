b12a62a72c632cced7df8a2cfcfb5aa9ce56d172
Felipe Sierra <felipesierrae@gmail.com>
2021-11-26 17:26:17 -0500

    Correcion Autenticacion

--- graphql/auth/resolvers.js
+++ graphql/auth/resolvers.js
@@ -44,8 +44,24 @@ const resolversAutenticacion = {
       }
     },
 
-    validateToken: async (parent, args, context) => {
+    refreshToken: async (parent, args, context) => {
       console.log('contexto', context);
+      if (!context.userData) {
+        return {
+          error: 'token no valido',
+        };
+      } else {
+        return {
+          token: generateToken({
+            _id: context.userData._id,
+            nombre: context.userData.nombre,
+            apellido: context.userData.apellido,
+            identificacion: context.userData.identificacion,
+            correo: context.userData.correo,
+            rol: context.userData.rol,
+          }),
+        };
+      }
     },
   },
 };
